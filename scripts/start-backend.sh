e125140908fcf9181eb8162d4a43a47293efb09a
Gijsjan <agijsbro@gmail.com>
2019-01-31 13:00:44 +0100

    Only remove gekaaptebrieven containers

--- scripts/start-backend.sh
+++ scripts/start-backend.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 
-echo -e "\n\n"
-read -p "Removing running containers?" anwser
-echo -e "\n\n"
-
-if [ "$anwser" == "y" ] || ["$anwser" == ""]; then
-	docker stop $(docker ps -aq)
-	docker rm $(docker ps -aq)
-fi
+# Stop and remove running Gekaapte Brieven containers
+CONTAINERS=$(docker ps -aq -f "name=gekaaptebrieven*")
+docker stop $CONTAINERS
+docker rm $CONTAINERS
 
 # docker-compose -p gekaaptebrieven -f docker/production.yml -f docker/development.yml pull
 # docker-compose -p gekaaptebrieven -f docker/production.yml -f docker/development.yml up --build
